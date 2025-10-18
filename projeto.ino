8cfe8874b906a643a164a1dbe576cf4ec4c5cf62
vihzxp <iasminvitoria140@gmail.com>
2025-10-18 02:21:17 -0300

    Update projeto.ino
    erros no código atualizados

--- projeto.ino
+++ projeto.ino
@@ -1,65 +1,82 @@
-//Variáveis Globais
-//função and/xor
+// Variáveis Globais
+
+// AND
 int pin_saidaAnd = 13;
-int pin_A_And=0;
-int pin_B_And=1;
-//or
+int pin_A_And = 0;
+int pin_B_And = 1;
+
+// OR
 int pin_saidaOr = 10;
 int pin_A_Or = 2;
 int pin_B_Or = 3;
-//not
+
+// NOT
 int pin_saidaNot = 8;
 int pin_A_Not = 4;
 
+// XOR
+int pin_saidaXor = 12;
+int pin_A_Xor = 5;
+int pin_B_Xor = 6;
+
 void setup()
 {
+  // AND
   pinMode(pin_saidaAnd, OUTPUT);
-  pinMode(pin_A_And,INPUT);
-  pinMode(pin_B_And,INPUT);
-  //Or
-  pinMode(pin_saidaOr, OUTPUT);
-  pinMode(pin_A_Or,INPUT);
-  pinMode(pin_B_Or,INPUT);
-  //not
-  pinMode(pin_saidaNot, OUTPUT);
-  pinMode(pin_A_Not,INPUT);
-}
+  pinMode(pin_A_And, INPUT);
+  pinMode(pin_B_And, INPUT);
 
+  // OR
+  pinMode(pin_saidaOr, OUTPUT);
+  pinMode(pin_A_Or, INPUT);
+  pinMode(pin_B_Or, INPUT);
+
+  // NOT
+  pinMode(pin_saidaNot, OUTPUT);
+  pinMode(pin_A_Not, INPUT);
+
+  // XOR
+  pinMode(pin_saidaXor, OUTPUT);
+  pinMode(pin_A_Xor, INPUT);
+  pinMode(pin_B_Xor, INPUT);
+}
 void loop()
 {
-  bool S = portaXor(digitalRead(pin_A_And),digitalRead(pin_B_And));
-  digitalWrite(pin_saidaAnd,S);
-  bool S2 = portaOr(digitalRead(pin_A_Or),digitalRead(pin_B_Or));
-  digitalWrite(pin_saidaOr,S2);
-  bool S3 = portaNot(digitalRead(pin_A_Not));
-  digitalWrite(pin_saidaNot,S3);
+  // AND
+  bool S_and = portaAnd(digitalRead(pin_A_And), digitalRead(pin_B_And));
+  digitalWrite(pin_saidaAnd, S_and);
+
+  // OR
+  bool S_or = portaOr(digitalRead(pin_A_Or), digitalRead(pin_B_Or));
+  digitalWrite(pin_saidaOr, S_or);
+
+  // NOT
+  bool S_not = portaNot(digitalRead(pin_A_Not));
+  digitalWrite(pin_saidaNot, S_not);
+
+  // XOR
+  bool S_xor = portaXor(digitalRead(pin_A_Xor), digitalRead(pin_B_Xor));
+  digitalWrite(pin_saidaXor, S_xor);
 }
-//E
+// Funções das portas lógicas
+// AND
 bool portaAnd(bool A, bool B)
 {
- 	if(A==true && B==true)
-      	return true;
-  	else
-      	return false;
+  return (A && B);
 }
-//Ou
+// OR
 bool portaOr(bool A, bool B)
 {
-  if(A == true || B == true)
-    return true;
-  else
-    return false;
+  return (A || B);
 }
-//not
+// NOT
 bool portaNot(bool A)
 {
-  if(A == true)
-    return false;
-  else 
-    return true;
+  return !A;
 }
-//shor
-bool portaXor(bool A, bool B){
+// XOR
+bool portaXor(bool A, bool B)
+{
   bool S1 = portaNot(A);
   bool S2 = portaNot(B);
   bool S3 = portaAnd(S1, B);
